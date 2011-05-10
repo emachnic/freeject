d727023b6a11920fe059129837e2ff5ea18f8475
Evan Machnic <emachnic@broadmac.net>
2011-05-10 11:23:36 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freeject::Application.config.secret_token = 'a8d885eee9ba894e79eb1667025ecb6b6e64b76f2c6efd136067c5c1b66d001efc2d7b94023b32689a55de8a98f18dae76416347823838589af6673c200ef295'
